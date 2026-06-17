25e1134cd6d28f17a01e4b300d6e26671abea386
Sri Lakshmi Sivani Dadi <dadisrilakshmisivani@gmail.com>
2026-06-17 13:19:13 +0530

    WIGEFC

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
-import Login from './Login'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import Dashboard from "./Dashboard";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <Login />
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
