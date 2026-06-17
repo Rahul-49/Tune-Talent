1c43f8594ce9c6751da7cb72f490ea9b1dc8d12e
Sri Lakshmi Sivani Dadi <dadisrilakshmisivani@gmail.com>
2026-06-17 09:06:02 +0530

    Updated login page

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 
 function Login() {
+  const [username, setusername] = useState("");
+  const [password, setPassword] = useState("");
+ const handleLogin = (e) => {
+    e.preventDefault();
+
+    console.log(username);
+    console.log(password);
+};
   return (
     <div>
       <h2>Login</h2>
-
-      <form>
-        <input type="text" placeholder="Enter Username"/>
-        <input type="password" placeholder="Enter your password"/>
-        <button type="submit"> Login</button>
+<form onSubmit={handleLogin}>
+<input type="text" placeholder="Enter Username" value={username} onChange={(e) => setusername(e.target.value)} />
+ <input type="password" placeholder="Enter your password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+        <button type="submit">  Login</button>
       </form>
     </div>
   );
